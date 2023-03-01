a9cab80eec9edea00cf1d771818abe8913a9f199
Paulo Lopes Estevão <pl1745240@gmail.com>
2023-03-01 15:49:20 +0100

    feat: :sparkles: Adiciona fucionalidade na entidade LocationType

--- domain/entities/location_type.go
+++ domain/entities/location_type.go
@@ -1,1 +1,52 @@
 package entities
+
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
+
+type LocationType struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at" valid:"-"`
+	UpdatedAt time.Time `json:"updated_at" valid:"-"`
+}
+
+func NewLocattionType(name string) (*LocationType, error) {
+	locationType := LocationType{
+		Name: name,
+	}
+	locationType.ID = uuid.NewV4().String()
+	locationType.CreatedAt = time.Now()
+	if err := locationType.isValid(); err != nil {
+		return nil, err
+	}
+	return &locationType, nil
+}
+
+func (locationType *LocationType) isValid() error {
+	_, err := govalidator.ValidateStruct(locationType)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (locationType *LocationType) SetUpdatedAt() {
+	locationType.UpdatedAt = time.Now()
+}
+
+func (locationType *LocationType) Update(name string) error {
+	locationType.Name = name
+	locationType.SetUpdatedAt()
+	if err := locationType.isValid(); err != nil {
+		return err
+	}
+	return nil
+}
