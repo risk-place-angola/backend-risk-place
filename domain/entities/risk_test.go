430f6190d193800136cf0ac922d6df400d861f96
Paulo Lopes Estevão <pl1745240@gmail.com>
2023-03-03 23:39:02 +0100

    test: Corrige testes adicionado novos campos.

--- domain/entities/risk_test.go
+++ domain/entities/risk_test.go
@@ -5,14 +5,17 @@ import (
 
 	"github.com/bxcodec/faker/v3"
 	"github.com/risk-place-angola/backend-risk-place/domain/entities"
+	uuid "github.com/satori/go.uuid"
 )
 
 func TestNewRisk(t *testing.T) {
 	risk, err := entities.NewRisk(&entities.Risk{
-		Name:        "Viana - Estalagem",
-		Latitude:    faker.Latitude(),
-		Longitude:   faker.Longitude(),
-		Description: "Homens armados assaltam a casas e estabelecimentos comerciais",
+		Name:           "Viana - Estalagem",
+		RiskTypeID:     uuid.NewV4().String(),
+		LocationTypeID: uuid.NewV4().String(),
+		Latitude:       faker.Latitude(),
+		Longitude:      faker.Longitude(),
+		Description:    "Homens armados assaltam a casas e estabelecimentos comerciais",
 	})
 	if err != nil {
 		t.Errorf("Erro ao criar um novo risco: %v", err)
