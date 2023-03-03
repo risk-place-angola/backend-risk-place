a661587850134ae52db615e713013409b4ec0638
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-03-03 14:09:55 +0100

    feat: :sparkles: Add feature risk type

--- domain/entities/risk_type.go
+++ domain/entities/risk_type.go
@@ -1,1 +1,19 @@
 package entities
+
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+)
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
+
+type RiskType struct {
+	ID          string    `json:"id" valid:"uuid,required~ O ID do tipo de risco é obrigatório."`
+	Name        string    `json:"name" valid:"required~ O nome do tipo de risco é obrigatório."`
+	Description string    `json:"description" valid:"required~ A descrição do tipo de risco é obrigatório."`
+	CreatedAt   time.Time `json:"created_at" valid:"-"`
+	UpdatedAt   time.Time `json:"updated_at" valid:"-"`
+}
