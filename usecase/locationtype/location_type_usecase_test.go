ebfde3f161a679569a580c057c78e1717d9888f2
Paulo Lopes Estevão <pl1745240@gmail.com>
2023-03-05 20:28:13 +0100

    test: Adiciona teste atualizar tipos de locais.

--- usecase/locationtype/location_type_usecase_test.go
+++ usecase/locationtype/location_type_usecase_test.go
@@ -4,11 +4,11 @@ import (
 	"testing"
 
 	"github.com/golang/mock/gomock"
+	"github.com/risk-place-angola/backend-risk-place/domain/entities"
 	"github.com/risk-place-angola/backend-risk-place/domain/repository/mocks"
 	"github.com/risk-place-angola/backend-risk-place/usecase/locationtype"
 	"github.com/stretchr/testify/assert"
 )
-
 
 func TestCreateLocationType(t *testing.T) {
 	ctrl := gomock.NewController(t)
@@ -23,4 +23,25 @@ func TestCreateLocationType(t *testing.T) {
 	})
 	assert.Nil(t, err)
 	assert.Equal(t, "Test", locationType.Name)
+}
+
+func TestUpdateLocationType(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	data := &entities.LocationType{
+		ID:   "20dabe23-3541-455b-b64d-3191f2b2a303",
+		Name: "Test",
+	}
+
+	mockLocationTypeRepository := mocks.NewMockLocationTypeRepository(ctrl)
+	mockLocationTypeRepository.EXPECT().FindByID(gomock.Any()).Return(data, nil)
+	mockLocationTypeRepository.EXPECT().Update(gomock.Any()).Return(nil)
+
+	locationTypeUseCase := locationtype.NewLocationTypeUseCase(mockLocationTypeRepository)
+	locationType, err := locationTypeUseCase.UpdateLocationType("20dabe23-3541-455b-b64d-3191f2b2a303", locationtype.UpdateLocationTypeDTO{
+		Name: "Test2",
+	})
+	assert.Nil(t, err)
+	assert.Equal(t, "Test2", locationType.Name)
 }
