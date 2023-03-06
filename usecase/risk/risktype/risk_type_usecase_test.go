2bcc831774d62f12745505ec1f36cecfe4d8aa33
Paulo Lopes Estevão <pl1745240@gmail.com>
2023-03-06 02:01:15 +0100

    test: Adiciona teste atualizar tipo de risco.

--- usecase/risk/risktype/risk_type_usecase_test.go
+++ usecase/risk/risktype/risk_type_usecase_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/golang/mock/gomock"
+	"github.com/risk-place-angola/backend-risk-place/domain/entities"
 	"github.com/risk-place-angola/backend-risk-place/domain/repository/mocks"
 	"github.com/risk-place-angola/backend-risk-place/usecase/risk/risktype"
 	"github.com/stretchr/testify/assert"
@@ -21,6 +22,32 @@ func TestRiskType(t *testing.T) {
 		riskType, err := riskTypeUseCase.CreateRiskType(&risktype.CreateRiskTypeDTO{
 			Name:        "Assalto",
 			Description: "Assalto a mão armada",
+		})
+		assert.Nil(t, err)
+		assert.Equal(t, "Assalto", riskType.Name)
+
+	})
+
+	t.Run("should update a risk type", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		data := &entities.RiskType{
+			ID:          "0c1baa42-3909-4bdb-837f-a80e68232ecd",
+			Name:        "Assalto",
+			Description: "Assalto a mão armada",
+		}
+
+		mockRiskTypeRepository := mocks.NewMockRiskTypeRepository(ctrl)
+		mockRiskTypeRepository.EXPECT().FindByID(gomock.Any()).Return(data, nil)
+		mockRiskTypeRepository.EXPECT().Update(gomock.Any()).Return(nil)
+
+		riskTypeUseCase := risktype.NewRiskTypeUseCase(mockRiskTypeRepository)
+		riskType, err := riskTypeUseCase.UpdateRiskType("0c1baa42-3909-4bdb-837f-a80e68232ecd", &risktype.UpdateRiskTypeDTO{
+			CreateRiskTypeDTO: risktype.CreateRiskTypeDTO{
+				Name:        "Assalto",
+				Description: "Assalto a mão armada com arma branca",
+			},
 		})
 		assert.Nil(t, err)
 		assert.Equal(t, "Assalto", riskType.Name)
