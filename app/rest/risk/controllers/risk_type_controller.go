20f2375b55237fb6038054e5b6bce5cf64dc13b6
Paulo Lopes Estevão <pl1745240@gmail.com>
2023-03-07 16:19:18 +0100

    feat: Adiciona feature criar tipos de riscos.

---
+++ app/rest/risk/controllers/risk_type_controller.go
@@ -0,0 +1,37 @@
+package risk_controller
+
+import (
+	"net/http"
+
+	"github.com/risk-place-angola/backend-risk-place/app/rest"
+	risk_presenter "github.com/risk-place-angola/backend-risk-place/app/rest/risk/presenter"
+	"github.com/risk-place-angola/backend-risk-place/usecase/risk/risktype"
+)
+
+type RiskTypeController interface {
+	RiskTypeCreateController(ctx risk_presenter.RiskPresenterCTX) error
+}
+
+type RiskTypeControllerImpl struct {
+	riskTypeUseCase risktype.RiskTypeUseCase
+}
+
+func NewRiskTypeController(riskTypeUseCase risktype.RiskTypeUseCase) RiskTypeController {
+	return &RiskTypeControllerImpl{
+		riskTypeUseCase: riskTypeUseCase,
+	}
+}
+
+func (controller *RiskTypeControllerImpl) RiskTypeCreateController(ctx risk_presenter.RiskPresenterCTX) error {
+	var risktype risktype.CreateRiskTypeDTO
+	if err := ctx.Bind(&risktype); err != nil {
+		return ctx.JSON(http.StatusBadRequest, rest.ErrorResponse{Message: err.Error()})
+	}
+
+	risktypeCreate, err := controller.riskTypeUseCase.CreateRiskType(&risktype)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, rest.ErrorResponse{Message: err.Error()})
+	}
+
+	return ctx.JSON(http.StatusCreated, risktypeCreate)
+}
