dbb98f6f78bd52dcb2392da3001724e56a9d4d4a
Paulo Lopes Estevão <pl1745240@gmail.com>
2023-03-07 04:01:39 +0100

    test: Adiciona teste e2e busca todos tipos locais

--- app/rest/locationtype/controllers/location_type_controller_e2e_test.go
+++ app/rest/locationtype/controllers/location_type_controller_e2e_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/labstack/echo/v4"
 	locationtype_controllers "github.com/risk-place-angola/backend-risk-place/app/rest/locationtype/controllers"
+	"github.com/risk-place-angola/backend-risk-place/domain/entities"
 	"github.com/risk-place-angola/backend-risk-place/domain/repository/mocks"
 	locationtype_usecase "github.com/risk-place-angola/backend-risk-place/usecase/locationtype"
 	"github.com/stretchr/testify/assert"
@@ -38,5 +39,34 @@ func TestLocationTypeController(t *testing.T) {
 			assert.Equal(t, http.StatusCreated, rec.Code, "error status code != 201")
 		}
 
+	})
+
+	t.Run("should return 200 when find all location types", func(t *testing.T) {
+		e := echo.New()
+		res := httptest.NewRequest("GET", "/api/v1/locationtype", nil)
+		rec := httptest.NewRecorder()
+		ctx := e.NewContext(res, rec)
+
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		data := []*entities.LocationType{
+			{
+				Name: "Risco",
+			},
+			{
+				Name: "Hospital",
+			},
+		}
+
+		mockLocationTypeRepository := mocks.NewMockLocationTypeRepository(ctrl)
+		mockLocationTypeRepository.EXPECT().FindAll().Return(data, nil)
+
+		locationTypeUseCase := locationtype_usecase.NewLocationTypeUseCase(mockLocationTypeRepository)
+		locationTypeController := locationtype_controllers.NewLocationTypeController(locationTypeUseCase)
+
+		if assert.NoError(t, locationTypeController.LocationTypeFindAllController(ctx)) {
+			assert.Equal(t, http.StatusOK, rec.Code, "error status code != 200")
+		}
 	})
 }
