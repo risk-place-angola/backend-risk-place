87d8c293d20327b15bbe8528577aa253aef617e2
Paulo Lopes Estevão <pl1745240@gmail.com>
2023-03-07 03:41:20 +0100

    feat: Adiciona feature controller criar location.

---
+++ app/rest/locationtype/controllers/location_type_controller.go
@@ -0,0 +1,37 @@
+package locationtype_controllers
+
+import (
+	"net/http"
+
+	"github.com/risk-place-angola/backend-risk-place/app/rest"
+	locationtype_presenter "github.com/risk-place-angola/backend-risk-place/app/rest/locationtype/presenter"
+	"github.com/risk-place-angola/backend-risk-place/usecase/locationtype"
+)
+
+type LocationTypeController interface {
+	LocationTypeCreateController(ctx locationtype_presenter.LocationTypePresenterCTX) error
+}
+
+type LocationTypeControllerImpl struct {
+	locationtypeUseCase locationtype.LocationTypeUseCase
+}
+
+func NewLocationTypeController(locationtypeUseCase locationtype.LocationTypeUseCase) LocationTypeController {
+	return &LocationTypeControllerImpl{
+		locationtypeUseCase: locationtypeUseCase,
+	}
+}
+
+func (controller *LocationTypeControllerImpl) LocationTypeCreateController(ctx locationtype_presenter.LocationTypePresenterCTX) error {
+	var locationType locationtype.CreateLocationTypeDTO
+	if err := ctx.Bind(&locationType); err != nil {
+		return ctx.JSON(http.StatusBadRequest, rest.ErrorResponse{Message: err.Error()})
+	}
+
+	locationTypeCreate, err := controller.locationtypeUseCase.CreateLocationType(locationType)
+	if err != nil {
+		return ctx.JSON(http.StatusInternalServerError, rest.ErrorResponse{Message: err.Error()})
+	}
+
+	return ctx.JSON(http.StatusCreated, locationTypeCreate)
+}
