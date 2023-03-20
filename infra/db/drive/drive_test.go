be28761d9bb37485ef1619708512fa42f95af390
Paulo Lopes Estevão <pl1745240@gmail.com>
2023-03-21 00:51:27 +0100

    test: Adiciona teste para implem da drive postgres

---
+++ infra/db/drive/drive_test.go
@@ -0,0 +1,53 @@
+package drive_test
+
+import (
+	"log"
+	"path/filepath"
+	"runtime"
+	"testing"
+
+	"github.com/joho/godotenv"
+	"github.com/risk-place-angola/backend-risk-place/infra/db"
+	"github.com/risk-place-angola/backend-risk-place/infra/db/drive"
+)
+
+func init() {
+	_, b, _, _ := runtime.Caller(0)
+	basepath := filepath.Dir(b)
+
+	err := godotenv.Load(basepath + "/../../../.env.example")
+
+	if err != nil {
+		log.Fatalf("Error loading .env files")
+	}
+}
+
+func TestDrivePostgres(t *testing.T) {
+	postgres := drive.NewPostgres()
+	driver := drive.Drive(postgres)
+
+	if driver == "" {
+		t.Error("driver is empty")
+	}
+
+	dns := postgres.Connect()
+
+	if dns == "" {
+		t.Error("dns is empty")
+	}
+
+	db, err := db.NewConnection(dns)
+	if err != nil {
+		t.Error(err)
+	}
+	if db == nil {
+		t.Error("db is nil")
+	}
+
+	dbConn := db.GetDatabaseConnection()
+	if dbConn == nil {
+		t.Error("dbConn is nil")
+	}
+
+	defer dbConn.Close()
+}
