d6ee4f991456cc4aaf07c1c040e6909c82a05c6e
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-03-29 12:00:09 +0100

    refactor: :recycle: refatorando o teste da entidade de usuario

--- domain/entities/user_test.go
+++ domain/entities/user_test.go
@@ -4,22 +4,22 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"golang.org/x/crypto/bcrypt"
 
 	"github.com/risk-place-angola/backend-risk-place/domain/entities"
 )
 
 func TestNewUser(t *testing.T) {
-	name := "any_name"
-	email := "any_email"
-	password := "any_password"
+	name := "John Doe"
+	email := "johndoe@example.com"
+	password := "secret123"
 	user, err := entities.NewUser(name, email, password)
 
 	assert.Nil(t, err, "Expected nil, got error %v", err)
 	assert.NotNil(t, user, "Expected user not to be nil")
-
 	assert.Equal(t, name, user.Name, "Expected %s, got %s", name, user.Name)
 	assert.Equal(t, email, user.Email, "Expected %s, got %s", email, user.Email)
-	assert.Equal(t, password, user.Password, "Expected %s, got %s", password, user.Password)
+	assert.True(t, bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)) == nil, "Passwords do not match")
 	assert.NotEmpty(t, user.ID, "Expected user ID to be non-empty")
 }
 
