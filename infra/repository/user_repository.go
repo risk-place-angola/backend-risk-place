97c87a323722175bba37dded2c00ec8c827d4d30
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-04-08 23:23:08 +0100

    refactor: :recycle: Refatorando metodo findByEmail

--- infra/repository/user_repository.go
+++ infra/repository/user_repository.go
@@ -1,8 +1,6 @@
 package repository
 
 import (
-	"errors"
-
 	"github.com/jinzhu/gorm"
 	"github.com/risk-place-angola/backend-risk-place/domain/entities"
 )
@@ -40,12 +38,7 @@ func (u *UserRepository) Delete(id string) error {
 }
 
 func (u *UserRepository) FindByEmail(email string) (*entities.User, error) {
-	var user entities.User
-	if err := u.Db.Where("email = ?", email).First(&user).Error; err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return nil, nil
-		}
-		return nil, err
-	}
-	return &user, nil
+	user := &entities.User{}
+	err := u.Db.Where("email = ?", email).First(user).Error
+	return user, err
 }
