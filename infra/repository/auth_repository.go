19366e0d6ea228ff880f602d5cf5936b68ffc4e4
Paulo Lopes Estevão <pl1745240@gmail.com>
2023-04-19 05:05:51 +0100

    feat: Adicionar repositorio de acesso ao banco de dados para entidade auth

---
+++ infra/repository/auth_repository.go
@@ -0,0 +1,39 @@
+package repository
+
+import (
+	"errors"
+	"github.com/jinzhu/gorm"
+	"github.com/risk-place-angola/backend-risk-place/domain/entities"
+)
+
+type AuthJWTRepository struct {
+	Db *gorm.DB
+}
+
+func NewAuthJWTRepository(db *gorm.DB) *AuthJWTRepository {
+	return &AuthJWTRepository{Db: db}
+}
+
+func (a *AuthJWTRepository) Save(entity *entities.Auth) error {
+	return a.Db.Save(entity).Error
+}
+
+func (a *AuthJWTRepository) FindByUsername(username string) (*entities.Auth, error) {
+	var entity entities.Auth
+	err := a.Db.Where("username = ?", username).First(&entity).Error
+	if err != nil {
+		return nil, errors.New("not found user")
+	}
+
+	return &entity, nil
+}
+
+func (a *AuthJWTRepository) FindUserIfExists() error {
+	var entity entities.Auth
+	var result int64
+	err := a.Db.Find(&entity).Count(&result).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
