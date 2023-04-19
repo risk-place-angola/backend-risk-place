fd83762fe38c2cfde40ac21819dde91ab99f35dc
Paulo Lopes Estevão <pl1745240@gmail.com>
2023-04-19 06:41:36 +0100

    fix: Corrigir bug na importacao do servico auth

--- api/rest/dependency/router_dependency.go
+++ api/rest/dependency/router_dependency.go
@@ -4,7 +4,6 @@ import (
 	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo/v4"
 	"github.com/risk-place-angola/backend-risk-place/api/rest/router"
-	"github.com/risk-place-angola/backend-risk-place/app/authjwt"
 )
 
 func DependencyRouter(db *gorm.DB, echo *echo.Echo) *echo.Echo {
@@ -14,7 +13,6 @@ func DependencyRouter(db *gorm.DB, echo *echo.Echo) *echo.Echo {
 		RiskTypeRouter:  RiskTypeDependency(db, echo),
 		PlaceRouter:     PlaceDependency(db, echo),
 		UserRouter:      UserDependency(db, echo),
-		IAuthService:    authjwt.AuthDependency(db),
 		Echo:            echo,
 	}
 
