cf6876d019d4429c6b138eadae4928d648db86e7
Paulo Lopes Estevão <pl1745240@gmail.com>
2023-05-29 20:29:20 +0100

    feat: add implementation service auths

--- app/authjwt/auth_jwt_service.go
+++ app/authjwt/auth_jwt_service.go
@@ -41,3 +41,20 @@ func (a *AuthService) Auth(ctx echo.Context) error {
 
 	return ctx.JSON(200, token)
 }
+
+// Auths is Authentication service
+// @Summary auths
+// @Description return all auths
+// @Tags Auth
+// @Accept  json
+// @Produce  json
+// @Success 200 {object} []entities.Auth
+// @Failure 401 {object} string
+// @Router /auths [get]
+func (a *AuthService) Auths(ctx echo.Context) error {
+	auths, err := a.IAuthAPI.Auths()
+	if err != nil {
+		return ctx.JSON(400, err.Error())
+	}
+	return ctx.JSON(200, auths)
+}
