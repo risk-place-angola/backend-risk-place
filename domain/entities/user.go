55c4fcc5fb1309770fbb3cb2527ec0ae033e7d97
Paulo Lopes Estevão <pl1745240@gmail.com>
2023-05-29 17:27:20 +0100

    feat: remove passwordEncrypt in user

--- domain/entities/user.go
+++ domain/entities/user.go
@@ -71,15 +71,3 @@ func (user *User) VerifyPassword(password string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
 	return err == nil
 }
-
-func (user *User) passwordEncrypt() error {
-	password, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
-
-	if err != nil {
-		return err
-	}
-
-	user.Password = string(password)
-	return nil
-
-}
