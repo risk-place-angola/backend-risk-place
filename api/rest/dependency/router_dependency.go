de87ac8ac37bc18881776a65470523fc5ac74d6d
Paulo Lopes Estevão <pl1745240@gmail.com>
2023-06-05 21:50:57 +0100

    chore: Remover dependencia Risk Place Type.

--- api/rest/dependency/router_dependency.go
+++ api/rest/dependency/router_dependency.go
@@ -10,12 +10,9 @@ import (
 func DependencyRouter(db *gorm.DB, echo *echo.Echo) *echo.Echo {
 
 	router_ := router.RouterImpl{
-		PlaceTypeRouter: PlaceTypeDependency(db, echo),
-		RiskTypeRouter:  RiskTypeDependency(db, echo),
-		PlaceRouter:     PlaceDependency(db, echo),
-		UserRouter:      UserDependency(db, echo),
-		IAuthService:    authjwt.AuthDependency(db),
-		Echo:            echo,
+		UserRouter:   UserDependency(db, echo),
+		IAuthService: authjwt.AuthDependency(db),
+		Echo:         echo,
 	}
 
 	router.NewRouter(&router_).Router()
