1e7761334391219c3ddf52bd3e15d3804ec3ba39
Paulo Lopes Estevão <pl1745240@gmail.com>
2023-06-06 02:11:33 +0100

    feat: Adicionar campo DeletedAt, este campo é utilizado pelo gorm para efectuar o SOFT DELETE

--- domain/entities/warning.go
+++ domain/entities/warning.go
@@ -3,21 +3,23 @@ package entities
 import (
 	"errors"
 	uuid "github.com/satori/go.uuid"
+	"gorm.io/gorm"
 	"time"
 )
 
 type Warning struct {
-	ID          string    `json:"id"`
-	ReportedBy  string    `json:"reported_by"`
-	User        *User     `json:"user"`
-	IsVictim    bool      `json:"is_victim"`
-	Fact        string    `json:"fact"`
-	Latitude    float64   `json:"latitude" valid:"required~ A latitude do risco é obrigatória."`
-	Longitude   float64   `json:"longitude" valid:"required~ A longitude do risco é obrigatória."`
-	IsFake      bool      `json:"is_fake"`
-	IsAnonymous bool      `json:"is_anonymous"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	ID          string         `json:"id"`
+	ReportedBy  string         `json:"reported_by"`
+	User        *User          `json:"user"`
+	IsVictim    bool           `json:"is_victim"`
+	Fact        string         `json:"fact"`
+	Latitude    float64        `json:"latitude" valid:"required~ A latitude do risco é obrigatória."`
+	Longitude   float64        `json:"longitude" valid:"required~ A longitude do risco é obrigatória."`
+	IsFake      bool           `json:"is_fake"`
+	IsAnonymous bool           `json:"is_anonymous"`
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdatedAt   time.Time      `json:"updated_at"`
+	DeletedAt   gorm.DeletedAt `json:"deleted_at" gorm:"index" valid:"-"`
 }
 
 func NewWarning(w *Warning) (*Warning, error) {
