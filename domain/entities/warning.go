5b400a8b5be8f1e4aa31e87dec999d6302da442e
Paulo Lopes Estevão <pl1745240@gmail.com>
2023-06-06 03:25:42 +0100

    feat: Adicionar campo StopAlerting como controle de alertas.

--- domain/entities/warning.go
+++ domain/entities/warning.go
@@ -8,29 +8,30 @@ import (
 )
 
 type Warning struct {
-	ID          string         `json:"id"`
-	ReportedBy  string         `json:"reported_by"`
-	User        *User          `json:"user"`
-	IsVictim    bool           `json:"is_victim"`
-	Fact        string         `json:"fact"`
-	Latitude    float64        `json:"latitude" valid:"required~ A latitude do risco é obrigatória."`
-	Longitude   float64        `json:"longitude" valid:"required~ A longitude do risco é obrigatória."`
-	IsFake      bool           `json:"is_fake"`
-	IsAnonymous bool           `json:"is_anonymous"`
-	CreatedAt   time.Time      `json:"created_at"`
-	UpdatedAt   time.Time      `json:"updated_at"`
-	DeletedAt   gorm.DeletedAt `json:"deleted_at" gorm:"index" valid:"-"`
+	ID           string         `json:"id"`
+	ReportedBy   string         `json:"reported_by"`
+	IsVictim     bool           `json:"is_victim"`
+	Fact         string         `json:"fact"`
+	Latitude     float64        `json:"latitude" valid:"required~ A latitude do risco é obrigatória."`
+	Longitude    float64        `json:"longitude" valid:"required~ A longitude do risco é obrigatória."`
+	IsFake       bool           `json:"is_fake"`
+	IsAnonymous  bool           `json:"is_anonymous"`
+	StopAlerting bool           `json:"stop_alerting"`
+	CreatedAt    time.Time      `json:"created_at"`
+	UpdatedAt    time.Time      `json:"updated_at"`
+	DeletedAt    gorm.DeletedAt `json:"deleted_at" gorm:"index" valid:"-"`
 }
 
 func NewWarning(w *Warning) (*Warning, error) {
 	warning := Warning{
-		ReportedBy:  w.ReportedBy,
-		IsVictim:    w.IsVictim,
-		Fact:        w.Fact,
-		Latitude:    w.Latitude,
-		Longitude:   w.Longitude,
-		IsFake:      w.IsFake,
-		IsAnonymous: w.IsAnonymous,
+		ReportedBy:   w.ReportedBy,
+		IsVictim:     w.IsVictim,
+		Fact:         w.Fact,
+		Latitude:     w.Latitude,
+		Longitude:    w.Longitude,
+		IsFake:       w.IsFake,
+		IsAnonymous:  w.IsAnonymous,
+		StopAlerting: w.StopAlerting,
 	}
 	warning.ID = uuid.NewV4().String()
 	warning.CreatedAt = time.Now()
