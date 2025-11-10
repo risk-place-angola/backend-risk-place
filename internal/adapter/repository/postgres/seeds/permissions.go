8ec51199d3d9f87208a6816585ca45ee28c375c6
Paulo Lopes Estevão <pl1745240@gmail.com>
2025-11-10 01:11:38 +0000

    feat: Add SeedPermissions function for seeding initial permission data

---
+++ internal/adapter/repository/postgres/seeds/permissions.go
@@ -0,0 +1,20 @@
+package seeds
+
+import (
+	"context"
+	"database/sql"
+)
+
+func SeedPermissions(ctx context.Context, db *sql.DB) error {
+	_, err := db.ExecContext(ctx, `
+	INSERT INTO permissions (resource, action)
+	VALUES
+		('user', 'create'),
+		('user', 'update'),
+		('user', 'delete'),
+		('report', 'create'),
+		('report', 'update'),
+		('report', 'delete');
+	`)
+	return err
+}
