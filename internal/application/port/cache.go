583acfc581b90e847327014f194bb145f4cc1562
Paulo Lopes Estevão <pl1745240@gmail.com>
2025-11-11 14:10:23 +0000

    refactor: Organize interface methods in cache.go for better readability

--- internal/application/port/cache.go
+++ internal/application/port/cache.go
@@ -6,17 +6,33 @@ import (
 )
 
 type Cache interface {
+	KVCache
+	HashCache
+	GeoCache
+	HealthChecker
+}
+
+type KVCache interface {
 	Get(ctx context.Context, key string) (string, error)
+	Set(ctx context.Context, key, value string, timer time.Duration) error
+	Delete(ctx context.Context, key string) error
+}
+
+type HashCache interface {
 	HGet(ctx context.Context, key, field string) (string, error)
 	HGetAll(ctx context.Context, key string) (map[string]string, error)
-	Set(ctx context.Context, key, value string, timer time.Duration) error
 	HSet(ctx context.Context, key, field, value string) error
-	Delete(ctx context.Context, key string) error
 	HDelete(ctx context.Context, key, field string) error
-	Ping(ctx context.Context) error
 	HExpire(ctx context.Context, key string, expiration time.Duration, fields ...string)
+}
+
+type GeoCache interface {
 	GeoAdd(ctx context.Context, key string, longitude, latitude float64, member string) error
-	GeoSearch(ctx context.Context, key string, longitude float64, latitude float64, radiusMeters float64) ([]string, error)
+	GeoSearch(ctx context.Context, key string, longitude, latitude, radiusMeters float64) ([]string, error)
+}
+
+type HealthChecker interface {
+	Ping(ctx context.Context) error
 }
 
 type InMemory interface {
