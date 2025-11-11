7aadb3b9a4bed5096fcc510a7786a6520e727d53
Paulo Lopes Estevão <pl1745240@gmail.com>
2025-11-11 14:10:49 +0000

    refactor: Simplify PostgreSQL connection logic and improve error handling

--- internal/infra/db/db.go
+++ internal/infra/db/db.go
@@ -4,46 +4,33 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
-	_ "github.com/lib/pq"
-	"github.com/risk-place-angola/backend-risk-place/internal/adapter/repository/postgres/seeds"
-	"github.com/risk-place-angola/backend-risk-place/internal/config"
 	"log"
 	"log/slog"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
+	"github.com/risk-place-angola/backend-risk-place/internal/adapter/repository/postgres/seeds"
+	"github.com/risk-place-angola/backend-risk-place/internal/config"
 )
 
-var DB *sql.DB
+const (
+	maxOpenConns    = 25
+	maxIdleConns    = 25
+	connMaxLifetime = 5 * time.Minute
+)
 
 func NewPostgresConnection(cfg config.Config) *sql.DB {
-
-	var dsn string
-
-	if cfg.DatabaseConfig.ConnString != "" {
-		slog.Info("Using custom PostgreSQL connection string")
-		dsn = cfg.DatabaseConfig.ConnString
-	} else {
-		slog.Info("Using PostgreSQL connection parameters")
-		if cfg.DatabaseConfig.Host == "" || cfg.DatabaseConfig.Port == "" || cfg.DatabaseConfig.User == "" || cfg.DatabaseConfig.Password == "" || cfg.DatabaseConfig.Name == "" {
-			slog.Error("Missing required PostgreSQL connection parameters")
-			log.Fatal("Error: Missing required PostgreSQL connection parameters")
-		}
-		dsn = fmt.Sprintf(
-			"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-			cfg.DatabaseConfig.Host, cfg.DatabaseConfig.Port, cfg.DatabaseConfig.User, cfg.DatabaseConfig.Password, cfg.DatabaseConfig.Name,
-		)
-	}
-
-	db, err := sql.Open("postgres", dsn)
+	db, err := Connect(cfg)
 	if err != nil {
-		slog.Error("Failed to open PostgreSQL connection", "error", err)
-		log.Fatalf("Error opening PostgreSQL connection: %v", err)
+		slog.Error("Failed to connect to PostgreSQL database", "error", err)
+		log.Fatalf("Error connecting to PostgreSQL database: %v", err)
 		os.Exit(1)
 	}
 
-	db.SetMaxOpenConns(25)
-	db.SetMaxIdleConns(25)
-	db.SetConnMaxLifetime(5 * time.Minute)
+	db.SetMaxOpenConns(maxOpenConns)
+	db.SetMaxIdleConns(maxIdleConns)
+	db.SetConnMaxLifetime(connMaxLifetime)
 
 	err = db.Ping()
 	if err != nil {
@@ -62,6 +49,39 @@ func NewPostgresConnection(cfg config.Config) *sql.DB {
 	}
 
 	slog.Info("PostgreSQL connection established successfully")
-	DB = db
 	return db
+}
+
+func ClosePostgresConnection(db *sql.DB) {
+	if db != nil {
+		err := db.Close()
+		if err != nil {
+			slog.Error("Failed to close PostgreSQL connection", "error", err)
+			log.Fatalf("Error closing PostgreSQL connection: %v", err)
+		} else {
+			slog.Info("PostgreSQL connection closed successfully")
+		}
+	}
+}
+
+func Connect(cfg config.Config) (*sql.DB, error) {
+	dsn := GetDBConnectionString(cfg)
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func GetDBConnectionString(cfg config.Config) string {
+	if cfg.DatabaseConfig.ConnString != "" {
+		return cfg.DatabaseConfig.ConnString
+	}
+
+	return fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		cfg.DatabaseConfig.Host, cfg.DatabaseConfig.Port, cfg.DatabaseConfig.User, cfg.DatabaseConfig.Password, cfg.DatabaseConfig.Name,
+	)
 }
